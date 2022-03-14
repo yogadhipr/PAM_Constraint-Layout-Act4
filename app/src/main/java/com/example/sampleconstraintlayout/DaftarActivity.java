9622ecb516b25f2790b57f71f43d6d7bba988742
Yoga Adhi Prasetya <yoga0287@gmail.com>
2022-03-15 01:36:48 +0700

    course Mobile Application Development - activity 4 - Constraint Layout

---
+++ app/src/main/java/com/example/sampleconstraintlayout/DaftarActivity.java
@@ -0,0 +1,62 @@
+package com.example.sampleconstraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class DaftarActivity extends AppCompatActivity{
+    //Deklarasi variabel dengan tipe data EditText
+    EditText edtNama, edtAlamat, edtEmail, edtPassword, edtrepass;
+
+    //Deklarasi variabel dengan tipe data floating action button
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        edtNama =findViewById(R.id.edNama);
+        edtAlamat =findViewById(R.id.edAlamat);
+        edtEmail =findViewById(R.id.edEmail);
+        edtPassword =findViewById(R.id.edPassword);
+        edtrepass =findViewById(R.id.edrepass);
+
+        fab =findViewById(R.id.fabSimpan);
+
+        fab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (edtNama.getText().toString().isEmpty() ||
+                        edtAlamat.getText().toString().isEmpty() ||
+                        edtEmail.getText().toString().isEmpty() ||
+                        edtPassword.getText().toString().isEmpty() ||
+                        edtrepass.getText().toString().isEmpty())
+                {
+                    Snackbar.make(view, "Wajib isi seluruh data!!!", Snackbar.LENGTH_LONG).show();
+                }else{
+                    if(edtPassword.getText().toString().equals(edtrepass.getText().toString()))
+                    {
+                        Toast.makeText(getApplicationContext(), "Pendaftaran berhasil",
+                                Toast.LENGTH_LONG).show();
+
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                    }else{
+                        Snackbar.make(view,"Password dan REpassword harus sama!",
+                                Snackbar.LENGTH_LONG).show();
+                    }
+                }
+            }
+
+        });
+
+    }
+}
--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.sampleconstraintlayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +22,25 @@ public class MainActivity extends AppCompatActivity {
 
     //deklarasi variabel untuk menyimpan email dan password
     String nama, password;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //method untuk menampilkan menu.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //Membuat kondisi jika yang dipilih adalah id mnDaftar
+        if (item.getItemId() == R.id.mnDftr)
+        {
+            //Method untuk memanggil activity "DaftarActivity"
+            Intent i = new Intent(getApplicationContext(), DaftarActivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 
     @Override
